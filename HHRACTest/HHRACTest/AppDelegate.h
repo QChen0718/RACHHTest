3d2bb442cac6305f0529754549ecad498d306d11
white <chenqing0718@163.com>
2019-06-28 20:06:00 +0800

    初始化项目

---
+++ HHRACTest/HHRACTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHRACTest
+//
+//  Created by White-C on 2019/6/28.
+//  Copyright © 2019 White-C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
